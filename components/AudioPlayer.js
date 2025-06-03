243341dacca795715e54194dc9e973beef861409
Kettengeschwindigkeit <666damned999@mail.ru>
2025-06-03 20:50:01 +0300

    Duration

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/AudioPlayer.module.css";
 import { BsArrowLeftShort } from "react-icons/bs";
 import { BsArrowRightShort } from "react-icons/bs";
@@ -7,14 +7,42 @@ import { FaPause } from "react-icons/fa";
 
 const AudioPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+
+  const audioPlayer = useRef();
 
   const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
+    const prevValue = isPlaying;
+
+    setIsPlaying(!prevValue);
+
+    if (!prevValue) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
   };
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
+
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+
+    setDuration(seconds);
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
 
   return (
     <div className={styles.audioPlayer}>
-      <audio src="https://moosic.my.mail.ru/file/f252ab577ebb48746966230658656d91.mp3" preload="metadata"></audio>
+      <audio ref={audioPlayer} src="https://moosic.my.mail.ru/file/f252ab577ebb48746966230658656d91.mp3"
+             preload="metadata"></audio>
       <button className={styles.forwardBackward}>
         <BsArrowLeftShort /> 30
       </button>
@@ -37,7 +65,7 @@ const AudioPlayer = () => {
       </div>
 
       {/* duration */}
-      <div className={styles.duration}>3:19</div>
+      <div className={styles.duration}>{(duration && !isNaN(duration)) && calculateTime(duration)}</div>
     </div>
   );
 };
